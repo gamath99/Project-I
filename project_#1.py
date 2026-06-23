399d0426308e381992c61b7dfefee30e8a6bb660
gamath99 <gamamathurin1@gmail.com>
2026-06-23 00:50:28 -0400

    Describe the program

---
+++ project_#1.py
@@ -0,0 +1,6 @@
+""" Project 1
+Gama Mathurin 
+The count and capture Game 
+The game consist in collecting more points then the opponents while moving the points in a clockwise way. The Player score by adding a point in 3 point cell.
+"""
+
