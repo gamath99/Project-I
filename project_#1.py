edc0bbf3ef308374540780b73df2d6a20f8d58c7
gamath99 <gamamathurin1@gmail.com>
2026-06-24 09:23:37 -0400

    Assign main variable for the program

--- project_#1.py
+++ project_#1.py
@@ -4,3 +4,15 @@ The count and capture Game
 The game consist in collecting more points then the opponents while moving the points in a clockwise way. The Player score by adding a point in 3 point cell.
 """
 
+#Declare the main variable that will be used through all the program
+
+#Declare variable that will store the score of each player 
+player1_score = 0
+player2_score = 0
+
+# Assign the initial Board that will start the game
+Board = [[4,4],[4,4],[4,4]]
+
+# Assign a variable for the turn player, 1 for player 1 and 2 for player 2
+turn_player = 1
+
