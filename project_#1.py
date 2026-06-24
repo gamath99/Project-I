06c3f0f9141e540de4a563d7d3c9e2f5d0357223
gamath99 <gamamathurin1@gmail.com>
2026-06-24 10:50:22 -0400

    Create Loop to start the game

--- project_#1.py
+++ project_#1.py
@@ -11,8 +11,48 @@ player1_score = 0
 player2_score = 0
 
 # Assign the initial Board that will start the game
-Board = [[4,4],[4,4],[4,4]]
+board = [[4,4],[4,4],[4,4]]
 
 # Assign a variable for the turn player, 1 for player 1 and 2 for player 2
 turn_player = 1
 
+#Create a path on how to move the points in the board 
+path = [(0,0),
+        (0,1),
+        (1,1),
+        (2,1),
+        (2,0),
+        (1,0)
+]
+
+#The game is a counting and repetition process to capture points to score. 
+# create a loop to play the game while consider the turn for each player 
+
+while True:
+    # Display the initial board 
+    print("\nToukay Board")
+    print("Row  Player_1    Player_2")
+
+    for i in range(len(board)):
+        print(i, "  ", board[i][0], "   ", board[i][1])
+
+    print("\nScores")
+    print("Player 1:", player1_score)
+    print("Player 2:", player2_score)
+    
+    #Check if the boardis empty as the game is over if it is empty
+    total = 0
+
+    for row in board:
+        total += row[0] + row[1]
+        
+    if total == 0
+        break
+
+    print("\nPlayer", turn_player)
+
+    row_selection = int(input("Choose row: "))
+    
+
+
+    
